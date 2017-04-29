2c700eabef19f8d6a77a961d9e70d68b90049447
edus44 <eduhhg@gmail.com>
2017-04-30 00:19:07 +0200

    revision 1

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-'use strict';
+'use strict'
 
-
-module.exports = require('./lib/middleware')
-module.exports.wrapper = require('./lib/wrapper')
-module.exports.ignore = module.exports.wrapper.ignore
+module.exports = require('./lib/loader')
+module.exports.errorHandler = require('./lib/errorHandler')
 module.exports.exception = require('./lib/exception')
-module.exports.handlers = require('./lib/handlers')
-module.exports.ResponseData = require('./lib/ResponseData')
-
-
-var emitter = require('./lib/emitter');
-
-module.exports.on = emitter.on.bind(emitter);
--- test/middleware.js
+++ test/middleware.js
@@ -1,346 +1,99 @@
-'use strict';
+'use strict'
 
 const expressDeliver = require('..')
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const exception = expressDeliver.exception
+const expect = require('chai').expect
+const sinon = require('sinon')
+const express = require('express')
+const request = require('supertest')
+const {exception} = expressDeliver
 
-const bluebird = require('bluebird');
-const q = require('q');
+function testIO(ctrl,test){
+    let app = express()
+    expressDeliver(app)
+    app.get('/',ctrl)
+    expressDeliver.errorHandler(app)
 
-function getRes(done){
-    var sendSpy = sinon.spy()
-    return {
-        sendSpy : sendSpy,
-        send : function(){
-            sendSpy.apply(null,arguments)
+    request(app).get('/').end(test)
+}
+
+describe('middlewares',()=>{
+
+    it('should respond normally',(done)=>{
+        testIO((req,res)=>{
+            res.send('hi')
+        },(err,res)=>{
+            expect(res.text).to.be.equal('hi')
             done()
-        },
-        status : sinon.spy()
-    }
-}
-
-function getArg(obj){
-    var call = obj.getCall(0);
-    return call && call.args[0]
-
-}
-
-function testIO(input,dataOutput,statusOutput){
-    var res = getRes(expectation);
-
-    expressDeliver({},res,nextValue=>{
-        if (nextValue){
-            res.deliver(nextValue)
-        }
+        })
     })
-    res.deliver(input)
 
-    function expectation(){
-        expect(res.sendSpy.calledOnce).to.be.true
+    it('should deliver success',(done)=>{
+        testIO(function*(){
+            return 'hi'
+        },(err,res)=>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).to.be.deep.equal({
+                status:true,
+                data:'hi'
+            })
+            done()
+        })
+    })
 
-        if (statusOutput){
-            expect(res.status.called).to.be.true
-            expect(res.status.args[0][0]).to.be.equal(statusOutput)
-        }else{
-            expect(res.status.called).to.be.false
-        }
-
-        expect(res.sendSpy.args[0][0]).to.be.deep.equal(dataOutput);
-    }
-}
-
-
-describe('expressDeliver() success',()=>{
-
-    before(()=>{
-        var res = {};
-        expressDeliver({},res,()=>{
-            expect(res.deliver).to.be.a('function')
-            expect(res.deliverSuccess).to.be.a('function')
-            expect(res.deliverError).to.be.a('function')
+    it('should deliver fail',(done)=>{
+        testIO(function*(){
+            throw new Error('foo')
+        },(err,res)=>{
+            expect(res.status).to.be.equal(500)
+            expect(res.body).to.be.deep.equal({
+                status:false,
+                error:{
+                    code:1000,
+                    message:'Internal error',
+                    data:'foo'
+                }
+            })
+            done()
         })
     })
 
 
-    it('should success with non null data',()=>{
-        var input = 0
-        var output = {
-            status:true,
-            data:0
-        }
-        testIO(input,output)
-    })
-
-    it('should success with expressDeliver.ResponseData',()=>{
-        var input = new expressDeliver.ResponseData({
-            data:'my data',
-            paginate: 'my extra param1',
-            metadata : 'my extra param2'
+    it('should deliver custom exception',(done)=>{
+        exception.define({
+            name:'CustomError',
+            code: 4000,
+            statusCode: 403,
+            message: 'This is a custom error',
         })
-        var output = {
-            status:true,
-            data:'my data',
-            paginate:'my extra param1',
-            metadata : 'my extra param2'
-        }
-        testIO(input,output)
+        testIO(function*(){
+            throw new exception.CustomError()
+        },(err,res)=>{
+            expect(res.status).to.be.equal(403)
+            expect(res.body).to.be.deep.equal({
+                status:false,
+                error:{
+                    code:4000,
+                    message:'This is a custom error'
+                }
+            })
+            done()
+        })
     })
 
-    it('should success with API Promise',()=>{
-
-        var input = Promise.resolve('my data')
-        var output = {
-            status:true,
-            data:'my data'
-        }
-        testIO(input,output)
-    })
-
-    it('should success with bluebird Promise',()=>{
-
-        var input = bluebird.resolve('my data')
-        var output = {
-            status:true,
-            data:'my data'
-        }
-        testIO(input,output)
-    })
-
-    it('should success with q Promise',()=>{
-
-        var input = q.resolve('my data')
-        var output = {
-            status:true,
-            data:'my data'
-        }
-        testIO(input,output)
-    })
-
-    it('should success with expressDeliver.ResponseData within Promise',()=>{
-        var input = Promise.resolve(new expressDeliver.ResponseData({
-            data:'my data',
-            extra: 'my extra param'
-        }))
-        var output = {
-            status:true,
-            data:'my data',
-            extra:'my extra param'
-        }
-        testIO(input,output)
-    })
-});
-
-
-
-
-describe('expressDeliver() fail',()=>{
-
-    it('should fail with empty data',()=>{
-        var input;
-        var output = {
-            status:false
-        }
-        testIO(input,output)
-    })
-
-
-    it('should fail with API Promise rejection',()=>{
-        var input = Promise.reject(Error('my error data'))
-        var output = {
-            status:false,
-            error:{
-                code:1003,
-                message:'Internal Error'
-            },
-            data : 'my error data'
-        }
-        testIO(input,output,500)
-    })
-
-
-    it('should fail with q Promise rejection',()=>{
-        var input = q.reject(Error('my error data'))
-        var output = {
-            status:false,
-            error:{
-                code:1003,
-                message:'Internal Error'
-            },
-            data : 'my error data'
-        }
-        testIO(input,output,500)
-    })
-
-    it('should fail with thenable',()=>{
-        var input = {
-            then : function(resolve){
-                resolve(Error('my error data'))
-            }
-        }
-        var output = {
-            status:false,
-            error:{
-                code:1003,
-                message:'Internal Error'
-            },
-            data : 'my error data'
-        }
-        testIO(input,output,500)
-    })
-
-
-    it('should fail with bluebird rejection',()=>{
-        var input = Promise.reject(Error('my error data'))
-        var output = {
-            status:false,
-            error:{
-                code:1003,
-                message:'Internal Error'
-            },
-            data : 'my error data'
-        }
-        testIO(input,output,500)
-    })
-
-    // it('should fail with async error',()=>{
-    //     var input = new Promise(function(resolve,reject){
-    //         setTimeout(function(){
-    //             throw Error('my error data')
-    //         })
-    //     })
-    //     var output = {
-    //         status:false,
-    //         error:{
-    //             code:1003,
-    //             message:'Internal Error'
-    //         },
-    //         data : 'my error data'
-    //     }
-    //     testIO(input,output,500)
-    // })
-
-
-    it('should fail with Error',()=>{
-        var input = new Error('Custom error');
-        var output = {
-            status:false,
-            error:{
-                code:1003,
-                message:'Internal Error'
-            },
-            data : 'Custom error'
-        }
-        testIO(input,output,500)
-    })
-
-    it('should fail with flat exception',()=>{
-
-        var input = exception
-        var output = {
-            status:false,
-            error:{
-                code:1000,
-                message:'Unknown exception'
-            },
-            data : undefined
-        }
-        testIO(input,output)
-    })
-
-    it('should fail with flat Error',()=>{
-
-        var input = Error
-        var output = {
-            status:false,
-            error:{
-                code:1001,
-                message:'Unknown Error'
-            },
-            data : undefined
-        }
-        testIO(input,output,500)
-    })
-
-
-    it('should fail with flat BaseException',()=>{
-
-        var input = exception.BaseException
-        var output = {
-            status:false,
-            error:{
-                code:1000,
-                message:'Unknown exception'
-            },
-            data : undefined
-        }
-        testIO(input,output)
-    })
-
-    it('should fail with new BaseException',()=>{
-        var input = new exception.BaseException(9999,'exception message','exception data');
-        var output = {
-            status:false,
-            error:{
-                code:9999,
-                message:'exception message'
-            },
-            data : 'exception data'
-        }
-        testIO(input,output)
-    })
-
-    it('should fail with flat custom exception',()=>{
-        var CustomException = class CustomException extends exception.BaseException{
-            constructor(data){
-                super(40001,'Custom Exception',data);
-            }
-        };
-
-        var input = CustomException
-        var output = {
-            status:false,
-            error:{
-                code:40001,
-                message:'Custom Exception'
-            },
-            data : undefined
-        }
-        testIO(input,output)
-    })
-
-    it('should fail with new custom exception',()=>{
-
-        var CustomException = class CustomException extends exception.BaseException{
-            constructor(data){
-                super(40001,'Custom Exception',data);
-            }
-        };
-
-        var input = new CustomException('custom data');
-        var output = {
-            status:false,
-            error:{
-                code:40001,
-                message:'Custom Exception'
-            },
-            data : 'custom data'
-        }
-        testIO(input,output)
-    })
-
-    it('should fail,data and statusCode with Error with statusCode',()=>{
-        var input = new Error('custom err');
-        input.statusCode = 401;
-        var output = {
-            status:false,
-            error:{
-                code:1003,
-                message:'Internal Error'
-            },
-            data : 'custom err'
-        }
-        testIO(input,output,401)
+    it('should deliver 404 with empty next',(done)=>{
+        testIO(function*(req,res,next){
+            next()
+        },(err,res)=>{
+            expect(res.status).to.be.equal(404)
+            expect(res.body).to.be.deep.equal({
+                status:false,
+                error:{
+                    code:1001,
+                    message:'Route not found'
+                }
+            })
+            done()
+        })
     })
 
 })
