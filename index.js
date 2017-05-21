48ab510b66d100933e478cfa20422df0de1f2a58
edus44 <eduhhg@gmail.com>
2017-05-21 13:49:01 +0200

    refactoring exception definitions

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 module.exports = require('./lib/mainLoader')
 module.exports.errorHandler = require('./lib/errorHandlerLoader')
-module.exports.exception = require('./lib/exception')
+module.exports.ExceptionPool = require('./lib/ExceptionPool')
 module.exports.ResponseData = require('./lib/ResponseData')
